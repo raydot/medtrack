be83961a11c68f7f39a076592f974943ea157e82
Dave Kanter <moatgoat@hotmail.com>
2026-04-07 22:12:42 -0700

    Added site keys to .env so backend.ts could be removed from .gitignore.

---
+++ backend/bin/backend.ts
@@ -0,0 +1,17 @@
+#!/usr/local/opt/node/bin/node
+import * as cdk from 'aws-cdk-lib/core';
+import { BackendStack } from '../lib/backend-stack';
+import { CIStack } from '../lib/ci-stack';
+
+const account = process.env.CDK_DEFAULT_ACCOUNT;
+const region = process.env.CDK_DEFAULT_REGION;
+
+const app = new cdk.App();
+
+new BackendStack(app, 'BackendStack', {
+  env: { account: account, region: region },
+});
+
+new CIStack(app, 'MedTrackCIStack', {
+  env: { account: account, region: region },
+});
